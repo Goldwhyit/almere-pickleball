86f944b81998d3495fa194c6519f4be664ee72cf
DHLoy <dhloy@MacBook-Pro-van-DHLoy-2.local>
2026-07-05 23:25:42 +0200

    fix: drop deprecated baseUrl from tsconfigs, set backend global api prefix

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({ origin: '*', credentials: true });
+  app.setGlobalPrefix('api');
 
   const port = process.env.PORT || 3000;
   await app.listen(port);
